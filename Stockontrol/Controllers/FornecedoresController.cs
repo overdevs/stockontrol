64de664061422d1fa80914311f411965e94595d5
Saulo <saulojoabps@gmail.com>
2019-05-20 21:18:45 -0300

    created controllers + fixed hrefs

---
+++ Stockontrol/Controllers/FornecedoresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Stockontrol.Controllers
+{
+    public class FornecedoresController : Controller
+    {
+        // GET: Fornecedores
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
