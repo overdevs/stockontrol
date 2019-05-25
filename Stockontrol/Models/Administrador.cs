41b96421a07f83dceba749834b338d455cbaeb7e
Saulo <saulojoabps@gmail.com>
2019-05-25 04:54:31 -0300

    creating models

---
+++ Stockontrol/Models/Administrador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stockontrol.Models
+{
+    public class Administrador
+    {
+        public long Id { get; set; }
+        public string Nome { get; set; }
+
+        public long UsuarioID { get; set; }
+    }
+}
---
+++ Stockontrol/Models/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Stockontrol.Models
+{
+    public class Produto
+    {
+        // id, Nome, Categoria, Preço, Quantidade, {idFornecedor}.
+        public long Id { get; set; }
+        public string Nome { get; set; }
+        public string Categoria { get; set; }
+        public float Preco { get; set; }
+        public int Quantidade { get; set; }
+        
+        public long idFornecedor { get; set; }
+    }
+}
