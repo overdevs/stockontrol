576f0dce17e97cf8633d376e234d40014a2d7313
Joao Alves <jneto2727@gmail.com>
2019-06-03 21:38:32 -0300

    Homescreen

--- Timoja/Controllers/HomeController.cs
+++ Timoja/Controllers/HomeController.cs
@@ -1,17 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Stockontroll.DAO;
+using Stockontroll.Models;
 
 namespace Timoja.Controllers
 {
     public class HomeController : Controller
     {
+        private StockontrolContext db = new StockontrolContext();
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(db.Produtos.ToList());
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }
