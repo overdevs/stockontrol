53d7dbf9ed57d909af520f4460fd680a346cee24
Joao Alves <jneto2727@gmail.com>
2019-06-06 20:01:47 -0300

    user Section mais ou menos adicionada

--- Timoja/Controllers/ContaController.cs
+++ Timoja/Controllers/ContaController.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
+using Stockontroll.DAO;
+using Stockontroll.Models;
 
 namespace Timoja.Controllers
 {
     public class ContaController : Controller
     {
-        
+
+        private StockontrolContext db = new StockontrolContext();
+
         public ActionResult MinhaConta()
         {
             return View();
@@ -18,9 +21,47 @@ namespace Timoja.Controllers
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Cadastro([Bind(Include = "Id,Email,Senha")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                // Configurando como Usuário Cmomum
+                usuario.Tipo = 0;
+                db.Usuarios.Add(usuario);
+                db.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(usuario);
+        }
+
         public ActionResult Login()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "Email, Senha")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                List<Usuario> resultQuery = db.Usuarios.ToList().FindAll(u => (u.Email == usuario.Email) && (u.Senha == usuario.Senha));
+                if (resultQuery.Count == 1)
+                {
+                    Session["id"] = resultQuery[0].Id;
+                    Session["email"] = resultQuery[0].Email;
+                    Session["senha"] = resultQuery[0].Senha;
+                    Session["tipo"] = resultQuery[0].Tipo;
+                    Session["usuario"] = resultQuery[0];
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+            return View(usuario);
         }
     }
 }
