641c68b1d4299015dfd1e3de67cc2fe1fb8c98b4
Saulo <saulojoabps@gmail.com>
2019-06-18 12:11:03 -0300

    fixes

---
+++ Timoja/Migrations/201906181459517_removingCliente.cs
@@ -0,0 +1,16 @@
+namespace Stockontroll.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class removingCliente : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
